4fe6b5082bd7e94f1ab89549a0686e79cdf41746
Arya <arya721@gmail.com>
2019-09-02 04:30:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(countries_in_western_africa, next_country)
   next_country = "violet"
   countries_in_western_africa.push(next_country)
 end
+
+def using_unshift(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = "violet"
+  countries_in_western_africa.unshift(next_country)
+end
