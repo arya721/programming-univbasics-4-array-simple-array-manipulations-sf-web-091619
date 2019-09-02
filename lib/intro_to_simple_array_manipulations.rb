0d775f41332d6d0a44475b51d077a04ca5f0ba72
Arya <arya721@gmail.com>
2019-09-02 05:34:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ def using_pop(continents)
 end
 #code ends
 
-def using_concat(my_favorite_things, morefavs)
+def using_concat(my_favorite_things, morefavs. all_my_favs)
 my_favorite_things = ["flatiron school"]
 morefavs = ["raindrops on roses", "whiskers on kittens", "sports cars"]
 all_my_favs.concat
