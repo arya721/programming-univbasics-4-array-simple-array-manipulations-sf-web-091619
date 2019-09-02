a7457d58ba4dac5b68dc7cdcf04c0f5407e9248d
Arya <arya721@gmail.com>
2019-09-02 04:28:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,5 @@
-def using_push (array, "string")
-using_push.push("next_color")
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = "Niger"
+  countries_in_western_africa.push(next_country)
+end
