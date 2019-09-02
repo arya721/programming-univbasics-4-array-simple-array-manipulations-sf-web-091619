761d49f5bc32b4e05792d2abc3f0db9a9805572c
Arya <arya721@gmail.com>
2019-09-02 05:08:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ end
 def using_pop(continents)
   continents = ["g", "Carpe ", "g", "Benin", "Carpe Verde", "Antarctica"]
   continents.pop
-  continents.length - 1
+  continents.length = - 1
 end
 
 
