00defa1b838df39ccbb012c300218398192dd635
Arya <arya721@gmail.com>
2019-09-02 06:41:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,6 +39,5 @@ end
 
 
 def using_uniq(countries_in_western_africa, next_country)
-  countries_in_western_africa.uniq = ["Benin", "Benin", "Gambia", "Liberia"]
-  countries_in_western_africa.uniq(countries_in_western_africa)
+  countries_in_western_africa.uniq
 end
