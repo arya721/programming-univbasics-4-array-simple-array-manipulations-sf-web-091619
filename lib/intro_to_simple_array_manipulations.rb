014138a4a306f7dc9de76e4cb6c10c99fa4705a1
Arya <arya721@gmail.com>
2019-09-02 04:45:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,3 @@ def using_unshift(countries_in_western_africa, next_country)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   countries_in_western_africa.unshift("Staten Island" )
 end
-
