207e471bc7283e01de9f9f569834ed2e71099a8c
Arya <arya721@gmail.com>
2019-09-03 04:19:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,7 @@ def using_uniq(countries_in_western_africa)
 end
 
 #edit below code
-def using_delete(famous_robots, string)
+def using_delete(array, famous_robots)
   famous_robots.delete
 end
 #code ends
