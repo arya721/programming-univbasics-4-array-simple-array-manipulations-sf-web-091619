192c4094baff655de5e471c993ad65730c720f20
Arya <arya721@gmail.com>
2019-09-03 04:37:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,10 +27,8 @@ def using_pop(continents)
 end
 
 def using_concat(my_favorite_things, more_favs)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
-  @all_my_favs = @my_favorite_things.concat(more_favs)
-end
+  my_favorite_things.concat(more_favs)
+
 
 
 def using_uniq(countries_in_western_africa)
